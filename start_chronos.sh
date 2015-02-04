fa5cc8c7cff45ca3546e63972af3335dcdeecb0c
Boris Shimanovsky <boris@factual.com>
2015-02-04 14:25:50 -0800

    fixing readme and startup script to pass in correct master args

--- start_chronos.sh
+++ start_chronos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos com.airbnb.scheduler.Main --zk_hosts $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port 4400
+java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos com.airbnb.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port 4400
