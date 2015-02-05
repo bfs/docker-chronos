8ec29b0e532f40cd83a2bc641af5967aa73449e0
Boris Shimanovsky <boris@factual.com>
2015-02-04 19:06:49 -0800

    putting env on one line

--- start_chronos.sh
+++ start_chronos.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-LIBPROCESS_PORT=8081
-LIPROCESS_IP=$CHRONOS_IP
-java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos com.airbnb.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port 8081
+LIBPROCESS_PORT=8081 LIPROCESS_IP=$CHRONOS_IP java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos com.airbnb.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port 8081
