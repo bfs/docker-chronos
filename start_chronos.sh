2d71c9d71678b756c023ac381e43712da4ac8503
Boris Shimanovsky <boris@factual.com>
2015-02-17 14:21:41 -0800

    updating for newer version

--- start_chronos.sh
+++ start_chronos.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-LIPROCESS_IP=$CHRONOS_IP java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos com.airbnb.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port 4400
+
+CHRONOS_ZK=${CHRONOS_ZK:-"localhost:2181"}
+MESOS_ZK=${MESOS_ZK:-"zk://localhost:2181/mesos"}
+HTTP_PORT=4400
+
+java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $HTTP_PORT
