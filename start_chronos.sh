3447d342fa1bb898ae6520fbc4e9c61ed2de350e
Boris Shimanovsky <boris@factual.com>
2015-02-19 13:30:27 -0800

    making it easier to map your own ports

--- start_chronos.sh
+++ start_chronos.sh
@@ -4,8 +4,6 @@
 CHRONOS_ZK=${CHRONOS_ZK:-"localhost:2181"}
 MESOS_ZK=${MESOS_ZK:-"zk://localhost:2181/mesos"}
 CHRONOS_HOSTNAME=${CHRONOS_HOSTNAME:-`hostname`}
+CHRONOS_HTTP_PORT=${CHRONOS_HOSTNAME:-4400}
 
-#constants
-HTTP_PORT=4400 #there are some bugs where chronos references other servers on the internal container port, so keeping this manual rather than setting via environment variables
-
-java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $HTTP_PORT
+java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $CHRONOS_HTTP_PORT
