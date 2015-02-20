ced11104a6781ec39cb9e2d696d3ba2ee254b4f6
Boris Shimanovsky <boris@factual.com>
2015-02-19 22:10:09 -0800

    fixed hostname stomping on port

--- start_chronos.sh
+++ start_chronos.sh
@@ -4,6 +4,6 @@
 CHRONOS_ZK=${CHRONOS_ZK:-"localhost:2181"}
 MESOS_ZK=${MESOS_ZK:-"zk://localhost:2181/mesos"}
 CHRONOS_HOSTNAME=${CHRONOS_HOSTNAME:-`hostname`}
-CHRONOS_HTTP_PORT=${CHRONOS_HOSTNAME:-4400}
+CHRONOS_HTTP_PORT=${CHRONOS_HTTP_PORT:-4400}
 
 java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $CHRONOS_HTTP_PORT
