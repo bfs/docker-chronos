f50dc71cdf3ba99eb4fe48c16a78dc212cf39525
Boris Shimanovsky <boris@factual.com>
2015-02-24 16:54:23 -0800

    trying the libprocess_ip hack

--- start_chronos.sh
+++ start_chronos.sh
@@ -11,5 +11,5 @@ echo "-------Chronos Host ENV Variables-------"
 env
 echo "----end Chronos Host ENV Variables----"
 
-#LIBPROCESS_IP=$HOST
+[ -n "$HOST" ] && LIBPROCESS_IP=$HOST
 java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $CHRONOS_HTTP_PORT
