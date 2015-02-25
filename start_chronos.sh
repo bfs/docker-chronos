3864546cb2d3c1609c13ecd4e621bc4f494d8ad8
Boris Shimanovsky <boris@factual.com>
2015-02-24 16:11:29 -0800

    adding some environment variables to startup output

--- start_chronos.sh
+++ start_chronos.sh
@@ -6,4 +6,9 @@ MESOS_ZK=${MESOS_ZK:-"zk://localhost:2181/mesos"}
 CHRONOS_HOSTNAME=${CHRONOS_HOSTNAME:-${HOSTNAME:-${HOST:-`hostname`}}}
 CHRONOS_HTTP_PORT=${CHRONOS_HTTP_PORT:-${PORT:-4400}}
 
+
+echo "-------Chronos Host ENV Variables-------"
+env
+echo "----end Chronos Host ENV Variables----"
+
 java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main --zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $CHRONOS_HTTP_PORT
