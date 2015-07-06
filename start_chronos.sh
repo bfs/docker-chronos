42bd0fbf258d49bcac919611cc0a1d67c7d08314
Boris Shimanovsky <boris@factual.com>
2015-07-06 12:59:23 -0700

    cleaning up some blank variable handling

--- start_chronos.sh
+++ start_chronos.sh
@@ -8,15 +8,15 @@ CHRONOS_HTTP_PORT=${CHRONOS_HTTP_PORT:-${PORT:-4400}}
 MAIL_SERVER=${MAIL_SERVER:-"localhost:25"}
 MAIL_FROM=${MAIL_FROM:-"chronos@$CHRONOS_HOSTNAME"}
 
-ARGS="--zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $CHRONOS_HTTP_PORT --mail_server $MAIL_SERVER --mail_from $MAIL_FROM"
+ARGS="--zk_hosts $CHRONOS_ZK --master $MESOS_ZK --hostname $CHRONOS_HOSTNAME --http_port $CHRONOS_HTTP_PORT --mail_server $MAIL_SERVER --mail_from \"$MAIL_FROM\""
 
-if [ -n $MAIL_PASSWORD ]; then
-  ARGS="$ARGS --mail_password $MAIL_PASSWORD"
+if [ -n "$MAIL_PASSWORD" ]; then
+  ARGS="$ARGS --mail_password \"$MAIL_PASSWORD\""
 fi
-if [ -n $MAIL_USER ]; then
-  ARGS="$ARGS --mail_user $MAIL_USER"
+if [ -n "$MAIL_USER" ]; then
+  ARGS="$ARGS --mail_user \"$MAIL_USER\""
 fi
-if [ -n $MAIL_SSL ]; then
+if [ -n "$MAIL_SSL" ]; then
   ARGS="$ARGS --mail_ssl"
 fi
 
