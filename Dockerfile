827a5801d470686d6e153eb06490c6afe8d25468
Boris Shimanovsky <boris@factual.com>
2015-07-22 10:37:52 -0700

    trying to follow chronos dockerfile example

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM boritzio/docker-mesosphere-base
 
 RUN apt-get install -y chronos
 
-EXPOSE 4400
-
 ADD start_chronos.sh /etc/my_init.d/chronos.sh
--- start_chronos.sh
+++ start_chronos.sh
@@ -4,7 +4,7 @@
 CHRONOS_ZK=${CHRONOS_ZK:-"localhost:2181"}
 MESOS_ZK=${MESOS_ZK:-"zk://localhost:2181/mesos"}
 CHRONOS_HOSTNAME=${CHRONOS_HOSTNAME:-${HOST:-`hostname`}}
-CHRONOS_HTTP_PORT=${CHRONOS_HTTP_PORT:-${PORT:-4400}}
+CHRONOS_HTTP_PORT=${CHRONOS_HTTP_PORT:-${PORT:-8080}}
 MAIL_SERVER=${MAIL_SERVER:-"localhost:25"}
 MAIL_FROM=${MAIL_FROM:-"chronos@$CHRONOS_HOSTNAME"}
 
@@ -22,5 +22,4 @@ fi
 
 echo "Starting with args: $ARGS"
 
-[ -n "$HOST" ] && LIBPROCESS_IP=$HOST
 java -Xmx512m -Djava.library.path=/usr/local/lib:/usr/lib64:/usr/lib -cp /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main $ARGS
